5bed576002a32df5015e37b4a762583ff85e3422
Ivan Ruiz <ivruizl@alumnos.unex.es>
2026-03-04 13:05:19 +0000

    fix: Archivos faltantes.

---
+++ web/src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css'
+import { Route, Routes } from 'react-router';
+import { NavbarContainer } from './components/navbar-container.component.tsx';
+import { ParcelaRoute } from './routes/parcelas/parcela.route.tsx';
+import { LoginRoute } from './routes/auth/login.route.tsx';
+import { Toaster } from 'sonner';
+import { ParcelasRoute } from './routes/parcelas/parcelas.route.tsx';
+import { ResultadoRoute } from './routes/resultados/resultado.route.tsx';
+import { CompareRoute } from './routes/compare/compare.route.tsx';
+
+function App() {
+  return (
+    <div className="w-full min-h-screen flex flex-col bg-base-300">
+      <Toaster position="bottom-right" richColors />
+      <Routes>
+        <Route path="/" element={<NavbarContainer />}>
+          <Route path="/compare" element={<CompareRoute />} />
+          <Route path="/auth/login" element={<LoginRoute />} />
+          <Route path="/parcelas">
+            <Route index element={<ParcelasRoute />} />
+            <Route path=":id" element={<ParcelaRoute />} />
+          </Route>
+          <Route path="/resultados">
+            <Route path=":id" element={<ResultadoRoute />} />
+          </Route>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App

693dfeadf47c5191f5eaceb9b72d52ea1996fb5f
Ivan Ruiz <ivruizl@alumnos.unex.es>
2026-03-11 11:29:32 +0000

    fix: Valor absoluto en resultados clave.

--- src/compare/compare.service.ts
+++ src/compare/compare.service.ts
@@ -217,7 +217,7 @@ export class CompareService implements OnModuleInit, OnModuleDestroy {
     });
 
     const topImpacts = result.diff.impacto_total
-      .sort((a, b) => a.diff - b.diff)
+      .sort((a, b) => Math.abs(a.diff) - Math.abs(b.diff))
       .map((i) => ({
         ...i,
         amountRef: result.left.impacto_total.find(
